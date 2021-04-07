42431633247adeb79265bb32ae14290b2c86a146
Vijay5996 <vijayraj8266@gmail.com>
2021-04-07 11:21:30 -0700

    Updaed app with shop page, added collection preview and collectionItems

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage.component';
-
-const hatsPage = () => {
-  return(
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  )
-}
-
-const JacketsPage = () => {
-  return(
-    <div>
-      <h1>Jackets Page</h1>
-    </div>
-  )
-}
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={hatsPage} />
-        <Route path='/shop/jackets' component={JacketsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
